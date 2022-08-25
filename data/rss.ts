1f3a4b672e5c2cb260827fc3ec266d1e1b8d0a1a
CheryX <roxmb25@gmail.com>
2022-08-25 13:23:57 +0200

    Fixed a problem in RSS generator

--- data/rss.ts
+++ data/rss.ts
@@ -18,14 +18,32 @@ export default async function generateRSS() {
         copyright: `All rights reserved ${new Date().getFullYear()}, Maciej Musielik`,
     });
 
+    
     allBlogs.map((post) => {
+
+        let authorID: string = post.data.authors[0]
+
+        /* Explanation on why is there "as keyof typeof data.authors":
+         * 
+         * "data.authors[x]" expects that x is one of the authors id ("cheryx" | "electruuu") 
+         * "authorID" is a string
+         * 
+         * Since the IDE isn't sure if the "authorID" will be an actual author, it throws an error.
+         * To fix that we need to specify that the "authorID" is actually the type of "data.authors"
+         * so it doesn't throw an error.
+         * 
+         * Yes, TypeScript is easy, how did you know?
+         */
+
+        let author = data.authors[authorID as keyof typeof data.authors].name;
+
         feed.item({
             title: post.data.title,
             url: `${siteData.siteUrl}posts/${post.filePath.replace('.mdx', '')}`,
             date: post.data.date,
             description: post.data.excerpt,
             categories: post.data.tags,
-            author: data.authors[post.data.authors[0]].name
+            author
         });
     });
 
